a857e247c41899e01a5d9d801efc8cce9f2681d7
Slava <SlavaSysoltsev@gmx.de>
2016-03-20 15:28:25 +0100

    special layout for big screens, model binding temporarily disabled

--- app/src/main/java/de/slava/schoolaccounting/Main.java
+++ app/src/main/java/de/slava/schoolaccounting/Main.java
@@ -51,30 +51,30 @@ public class Main extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
-        if (findViewById(R.id.fragment_container) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            // Create a new Fragment to be placed in the activity layout
-            storage = new Storage();
-            model = storage.loadModel();
-            MainFragment mainFragment = new MainFragment();
-            mainFragment.initData(model);
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            mainFragment.setArguments(getIntent().getExtras());
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mainFragment).commit();
-        }
+//        // Check that the activity is using the layout version with
+//        // the fragment_container FrameLayout
+//        if (findViewById(R.id.fragment_container) != null) {
+//
+//            // However, if we're being restored from a previous state,
+//            // then we don't need to do anything and should return or else
+//            // we could end up with overlapping fragments.
+//            if (savedInstanceState != null) {
+//                return;
+//            }
+//
+//            // Create a new Fragment to be placed in the activity layout
+//            storage = new Storage();
+//            model = storage.loadModel();
+//            MainFragment mainFragment = new MainFragment();
+//            mainFragment.initData(model);
+//
+//            // In case this activity was started with special instructions from an
+//            // Intent, pass the Intent's extras to the fragment as arguments
+//            mainFragment.setArguments(getIntent().getExtras());
+//
+//            // Add the fragment to the 'fragment_container' FrameLayout
+//            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mainFragment).commit();
+//        }
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
